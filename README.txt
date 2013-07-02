01356bd8bcd8ce0c0c4679591424abe0cfcbe71d
Stefanie Chong <stefanie.chong@text100.com.my>
2013-07-02 13:34:46 +0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is an interactive infographic demo prototype, brought to you by The Digital Hub.
+
+http://www.text100.com/
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Stress Test</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+</head>
+<body>
+	<div class="header"></div>
+
+	<div class="welcome">
+		<h1>Welcome to the Stress Test</h1>
+		<p> This is a stress test website created by the #digitalhub. Kindly fill in your particular in the table below.</p>
+		<div class="wrap">
+			<form>
+				<label>Name</label>
+				<input name="name" placeholder="Type here">
+				<label>Region</label>
+				<select>
+					<option>--Select--</option>
+					<option value="APAC">APAC</option>
+					<option value="EMEA">EMEA</option>
+					<option value="NA">NA</option>
+				</select>
+				<label>Location</label>
+				<select>
+					<option>--Select--</option>
+					<option value="Malaysia">Malaysia</option>
+					<option value="Singapore">Singapore</option>
+				</select>
+				<label>Position</label>
+				<select>
+					<option>--Select--</option>
+					<option value="Manager">Manager</option>
+					<option value="Executive">Executive</option>
+				</select>
+				<label>Age Range</label>
+				<select>
+					<option>--Select--</option>
+					<option>20-25</option>
+					<option>25-30</option>
+				</select>
+				<label>E-mail Address</label>
+				<input name="email" type="email" placeholder="Type here">
+				<input id="submit" name="submit" type="submit" value="NEXT">
+			</form></div>
+		</div>
+
+	</body>
+	</html>
